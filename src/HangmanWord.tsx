11ba4682e0e2751f21f81063d54482b4f84c38e3
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-10-18 20:02:53 +0530

    completed markdown and styling for all components

---
+++ src/HangmanWord.tsx
@@ -0,0 +1,35 @@
+const HangmanWord = () => {
+  const word = "test";
+  const guessedLetters = ["t", "e"];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "0.25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => {
+        return (
+          <span style={{ borderBottom: ".1em solid black" }} key={index}>
+            <span
+              style={{
+                visibility: guessedLetters.includes(letter)
+                  ? "visible"
+                  : "hidden",
+              }}
+            >
+              {letter}
+            </span>
+          </span>
+        );
+      })}
+    </div>
+  );
+};
+
+export default HangmanWord;
